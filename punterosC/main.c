dfcbf7083d2b0e48efbc88e661d4ad9587ab31f7
nbgyjn <pablo1567@gmail.com>
2018-05-17 10:29:15 -0300

    nomenclatura punteros

--- punterosC/main.c
+++ punterosC/main.c
@@ -3,6 +3,23 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    //punteros
+    int *punteroNumero;
+    int numero;
+    int numeroDos;
+    numero=66;
+    numeroDos=&numero;
+    punteroNumero=&numero;//asignacion de memoria
+    printf("\n a- %d",numeroDos);
+    numeroDos=*punteroNumero;//asignacion de valor
+    printf("\n b- %d",numeroDos);
+    punteroNumero=*punteroNumero;
+    printf("%d\n",numero);
+    printf("%d\n",numeroDos);
+    printf("%d\n",&numero);
+    printf("\n d-%p",punteroNumero);
+    printf("\nc-%p",&punteroNumero);
+    printf("\n d- %d",*punteroNumero);
+    //printf("%d",*punteroNumero);
     return 0;
 }
